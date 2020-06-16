de7a734916a58599079090adf9a1b0e776265646
McMurphy Luo <McMurphy.Luo@gmail.com>
2020-06-16 22:52:13 +0800

    [Not compile] Add global tracker. Change class names. Decide to implement an vector which is trackable

--- dtrack.h
+++ dtrack.h
@@ -6,30 +6,71 @@
 #include <list>
 #include <memory>
 #include <array>
+#include <unordered_map>
+#include "signals.h"
 
 namespace dtrack
 {
   namespace detail
   {
-    struct NodeSharedBlockBase {
-      virtual void Invalidate() = 0;
+    class GlobalBlock {
+    private:
+      static GlobalBlock g_block;
+
+    public:
+      GlobalBlock()
+        : {
+
+      }
+
+      void FreeTracker() {
+
+      }
+
+      void Apply();
+
+      void CommitValidatedTracker() {
+
+      }
+
+      void CommitInvalidatedTracker() {
+
+      }
+
+      std::unordered_map<std::shared_ptr<>>
+      std::unique_ptr<std::uintptr_t[]> buffer;
     };
 
+    std::shared_ptr<GlobalBlock> GetGlobalBlock() {
+      static std::weak_ptr<GlobalBlock> instance;
+      if (std::shared_ptr<GlobalBlock> result = instance.lock()) {
+        return result;
+      }
+      std::shared_ptr<GlobalBlock> created_instance = std::make_shared<GlobalBlock>();
+      instance = created_instance;
+      return created_instance;
+    }
+
     struct ValueSharedBlockBase {
+      ValueSharedBlockBase()
+        : trackers_position() {
+
+      }
+
       virtual void Invalidate() = 0;
+      std::list<std::pair<size_t, uintptr_t>> trackers_position;
     };
 
     template<typename T>
-    struct NodeSharedBlock : public NodeSharedBlockBase
+    struct ValueSharedBlock : public ValueSharedBlockBase
     {
-      NodeSharedBlock(T default_value)
-        : value(default_value)
-        , values_bind_to_me() {
+      ValueSharedBlock(T default_value)
+        : value(default_value) {
 
       }
 
       virtual void Invalidate() override {
-        std::list<std::weak_ptr<detail::ValueSharedBlockBase>>::iterator it = values_bind_to_me.begin();
+        std::list<std::weak_ptr<detail::TrackerSharedBlockBase>>::iterator it = tracker_bind_to_me.begin();
         while (it != values_bind_to_me.end()) {
           if (it->expired()) {
             it = values_bind_to_me.erase(it);
@@ -42,7 +83,6 @@ namespace dtrack
       }
 
       T value;
-      std::list<std::weak_ptr<ValueSharedBlockBase>> values_bind_to_me;
     };
 
     template<typename T>
@@ -87,7 +127,7 @@ namespace dtrack
     }
 
     template<typename T, typename... N>
-    struct ValueSharedBlock : public ValueSharedBlockBase {
+    struct TrackerSharedBlock : public TrackerSharedBlockBase {
       ValueSharedBlock(const T& default_value, const std::function<T(const N&...)>& calculator)
         : value(default_value)
         , is_valid(true)
@@ -117,48 +157,80 @@ namespace dtrack
   }
 
   template<typename T>
-  class DNode {
+  class DValue {
   public:
-    DNode(T default_value)
-      : shared_block_(std::make_shared<detail::NodeSharedBlock<T>>(default_value)) {
+    DValue(const T& default_value)
+      : shared_block_(std::make_shared<detail::ValueSharedBlock<T>>(default_value)) {
 
     }
 
     void SetValue(T value) {
-      shared_block_->value = value;
-      shared_block_->Invalidate();
+      if (shared_block_->value != value) {
+        shared_block_->value = value;
+        shared_block_->Invalidate();
+      }
     }
 
     T Value() { return shared_block_->value; }
 
   public:
-    std::shared_ptr<detail::NodeSharedBlock<T>> shared_block_;
+    std::shared_ptr<detail::ValueSharedBlock<T>> shared_block_;
+  };
+
+  template<typename T>
+  class DVector {
+  public:
+
+
+  private:
+
   };
 
   template<typename T, typename... N>
-  class DValue {
+  class DTracker {
   public:
-    DValue(const T& default_value, const std::function<T(const N&...)>& calculator)
-      : shared_block_(std::make_shared<detail::ValueSharedBlock<T, N...>>(default_value, calculator))
+    DTracker(const T& default_value, const std::function<T(const N&...)>& calculator)
+      : shared_block_(std::make_shared<detail::TrackerSharedBlock<T, N...>>(default_value, calculator))
     {
 
     }
 
     template<size_t index>
-    DValue& Bind(const DNode<std::tuple_element_t<index, std::tuple<N...>>>& node) {
+    DTracker& Assemble(const DValue<std::tuple_element_t<index, std::tuple<N...>>>& node) {
       shared_block_->node_shared_block[index] = node.shared_block_;
       node.shared_block_->values_bind_to_me.push_back(shared_block_);
       shared_block_->Invalidate();
       return *this;
     }
 
+    template<size_t index>
+    DTracker& Assemble(const DTracker<std::tuple_element_t<index, std::tuple<N...>>>& tracker) {
+      shared_block_->node_shared_block[index] = node.shared_block_;
+      node.shared_block_->values_bind_to_me.push_back(shared_block_);
+      shared_block_->Invalidate();
+      return *this;
+    }
+
+    DTracker& Bind(const std::function<void(T)>& bind_function) {
+      shared_block_->node_shared_block[index] = node.shared_block_;
+      node.shared_block_->values_bind_to_me.push_back(shared_block_);
+      shared_block_->Invalidate();
+      return *this;
+    }
+
+    void Apply();
+
     T Value() {
       return shared_block_->Value();
     }
 
   private:
-    std::shared_ptr<detail::ValueSharedBlock<T, N...>> shared_block_;
+    std::shared_ptr<detail::TrackerSharedBlock<T, N...>> shared_block_;
   };
+
+  void Apply() {
+
+  }
 }
 
 #endif // DTRACK_
